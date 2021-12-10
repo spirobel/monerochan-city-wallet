3dd8955d13dd405b9ec830aaecf2686014d22d29
spirobel <spirobel@protonmail.com>
2021-12-11 01:25:48 +0800

    home component

---
+++ src/features/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Clubcards } from '../clubcards/Clubcards';
+
+function Home() {
+
+
+    return (
+        <div>
+            <Clubcards />
+        </div>
+    );
+}
+
+export default Home;
--- src/features/navigation/Navigation.jsx
+++ src/features/navigation/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { navigate, selectCurrentPage } from './navigation-slice'
 import { useDispatch, useSelector } from 'react-redux';
+import Home from '../home/Home';
 
 function Navigation() {
   let currentPage = useSelector(selectCurrentPage)
   const dispatch = useDispatch()
   let main = <div></div>;
   switch (currentPage) {
-    case 'root':
-      main = <div>root</div>
+    case 'home':
+      main = <Home />
       break;
     case 'test':
       main = <div>test</div>
@@ -19,7 +20,7 @@ function Navigation() {
     <div>
       monerochan
       {main}
-      <button onClick={() => dispatch(navigate("root"))}>root</button>
+      <button onClick={() => dispatch(navigate("home"))}>home</button>
       <button onClick={() => dispatch(navigate("test"))}>test</button>
 
     </div>
