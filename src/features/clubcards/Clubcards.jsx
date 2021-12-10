a3c5590288006c2f6adc7163057a0871e7c0b8be
spirobel <spirobel@protonmail.com>
2021-12-11 01:25:56 +0800

    clubcards

---
+++ src/features/clubcards/Clubcards.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import {
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+} from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+    rectSortingStrategy,
+} from '@dnd-kit/sortable';
+import { Clubcard } from './Clubcard';
+
+
+export function Clubcards() {
+    const [items, setItems] = useState(['1', '2', '3', '4', '5', '6', '7', '8']);
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
+    return (
+        <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+        >
+            <SortableContext
+                items={items}
+                strategy={rectSortingStrategy}
+            >
+                {items.map(id => <Clubcard key={id} id={id} />)}
+            </SortableContext>
+        </DndContext>
+    );
+
+    function handleDragEnd(event) {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            setItems((items) => {
+                const oldIndex = items.indexOf(active.id);
+                const newIndex = items.indexOf(over.id);
+
+                return arrayMove(items, oldIndex, newIndex);
+            });
+        }
+    }
+}
