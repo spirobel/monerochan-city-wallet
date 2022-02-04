ee958e8b519a12ff7b7d734695e1a570dd22024d
spirobel <spirobel@protonmail.com>
2022-02-04 21:13:53 +0800

    useCurrentWallet hook

---
+++ src/utils/useCurrentWallets.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useChromeStorageLocal } from 'use-chrome-storage';
+import { ACTIVE_WALLET, ALL_WALLET_KEYS } from "../pages/Background/background_app/createWalletSaga";
+import { storage } from './storage'
+const STORAGE_AREA = 'local';
+const useCurrentWallets = () => {
+    const [awk, setAWK] = useState([]);
+    const [aw, setAW] = useState({});
+    const [isPersistent, setIsPersistent] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        storage.get(ALL_WALLET_KEYS, [], STORAGE_AREA)
+            .then(res => {
+                console.log("GET ALLWALLET KEYS first time", res)
+                setAWK(res);
+                setIsPersistent(true);
+                setError('');
+            })
+            .catch(error => {
+                setIsPersistent(false);
+                setError(error);
+            });
+    }, [ALL_WALLET_KEYS, STORAGE_AREA]);
+
+    useEffect(() => {
+        storage.get(awk, [], STORAGE_AREA)
+            .then(res => {
+                console.log("GET ALLWALLETs first time", res)
+                setAW(res);
+                setIsPersistent(true);
+                setError('');
+            })
+            .catch(error => {
+                setIsPersistent(false);
+                setError(error);
+            });
+    }, [awk])
+
+    useEffect(() => {
+        const onChange = (changes, areaName) => {
+            if (areaName === STORAGE_AREA && ALL_WALLET_KEYS in changes) {
+
+                setAWK(changes[ALL_WALLET_KEYS].newValue);
+                setIsPersistent(true);
+                setError('');
+            }
+            for (let i in awk) {
+                if (areaName === STORAGE_AREA && awk[i] in changes) {
+                    setAW(prevAW => prevAW[awk[i]] = changes[awk[i]])
+                    setIsPersistent(true);
+                    setError('');
+                }
+            }
+
+        };
+        chrome.storage.onChanged.addListener(onChange);
+        return () => {
+            chrome.storage.onChanged.removeListener(onChange);
+        };
+    }, [awk, aw, STORAGE_AREA, ALL_WALLET_KEYS]);
+
+
+
+
+    return [awk, aw, isPersistent, error];
+}
+export default useCurrentWallets;
