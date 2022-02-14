fdaec9ffca16404e8aa7432e51709003c77d1398
spirobel <spirobel@protonmail.com>
2022-02-15 05:01:54 +0800

    switch wallet card list

--- src/features/switch-wallet/SwitchWallet.jsx
+++ src/features/switch-wallet/SwitchWallet.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useChromeStorageLocal } from 'use-chrome-storage';
-import { Form, Input, Button, Checkbox, Radio, List } from 'antd';
+import { Form, Input, Button, Checkbox, Radio, List, Card } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { dispatchBackground } from '../../utils/dispatchBackground';
 import { saveWallet } from '../../pages/Background/background_app/createWalletSaga';
-import { LeftCircleOutlined } from '@ant-design/icons';
+import { LeftCircleOutlined, ToTopOutlined } from '@ant-design/icons';
 import { navigate } from '../navigation/navigation-slice'
 import useCurrentWallets from '../../utils/useCurrentWallets';
+
 
 export default function CreateWallet() {
     const [draftWallet, setdraftWallet] = useChromeStorageLocal('wallet-draft');
@@ -19,7 +20,19 @@ export default function CreateWallet() {
                 size="small"
                 bordered
                 dataSource={awk}
-                renderItem={item => <List.Item>{aw[item]}</List.Item>}
+                renderItem={item => {
+                    let title = item;
+                    if (aw[item]) {
+                        title = aw[item].name
+                    }
+                    return (
+                        <Card title={title} style={{ 'margin-bottom': '1em' }}>Card content{console.log("INSIDE", aw[item])}</Card>
+
+                    )
+                }
+
+
+                }
             />
             <Button key="edit" onClick={() => dispatch(navigate("create-wallet"))} >create wallet</Button>
         </div>
