93964987d50c46b13d48d82cfffddfde2d615e14
spirobel <spirobel@protonmail.com>
2022-02-23 01:56:07 +0800

    walletcard wallet name works but with (theoretical memory leak)

--- src/features/menu/wallet/WalletCard.jsx
+++ src/features/menu/wallet/WalletCard.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useChromeStorageLocal } from 'use-chrome-storage';
 import { navigate, selectCurrentPage } from '../../navigation/navigation-slice'
 import { Card, Tooltip } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons'
 import styles from './WalletCard.module.scss'
 import classNames from 'classnames';
 import wallet from '../../../assets/img/wallet.jpg'
-
+import { ACTIVE_WALLET } from '../../../pages/Background/background_app/createWalletSaga';
 const { Meta } = Card;
 
 
 export function WalletCard(props) {
     const dispatch = useDispatch()
+    const [activeWalletName] = useChromeStorageLocal(ACTIVE_WALLET, "");
+    const [activeWallet, setActiveWallet] = useState({ name: " bla" })
+    useEffect(() => {
+        chrome.storage.local.get(activeWalletName, function (aw) {
+            setActiveWallet(value => Object.assign({}, aw[activeWalletName]))
+        })
+    }, [activeWallet, setActiveWallet, ACTIVE_WALLET])
 
     return (
         <Card
@@ -31,7 +39,7 @@ export function WalletCard(props) {
         >
             <Meta
                 avatar={<img src={wallet} style={{ width: "150px" }} />}
-                title="Wallet Name"
+                title={activeWallet.name}
                 description="This is your currently selected wallet"
             />
         </Card>
