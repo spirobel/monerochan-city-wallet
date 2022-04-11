6672419c2782ce748f6af25bd520cce71f5e3ff7
spirobel <spirobel@protonmail.com>
2022-04-11 14:04:02 +0800

    cleanup syncWalletSyncSaga and make it handle creation and opening of wallets

--- src/pages/Background/background_app/syncWalletSyncSaga.js
+++ src/pages/Background/background_app/syncWalletSyncSaga.js
@@ -1,34 +1,15 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
-import { loadWalletData, open_monero_wallet, monerojs, start_monero_wallet_sync } from "./moneroWalletUtils"
-import { storage } from '../../../utils/storage'
-
-
-function* openWallet(wallet_config, action) {
-    const { keysData, cacheData } = yield call(loadWalletData, action.payload.name)
-    let config = {
-        networkType: wallet_config.networkType,
-        password: wallet_config.password,
-        keysData,
-        cacheData
-    }
-    const monero_wallet = yield call(open_monero_wallet, config)
-    console.log("SERVER URI", wallet_config.serverUri)
-    let sh = yield call([monero_wallet, "getSyncHeight"])
-    console.log("synchheigt before daemon connection:", sh)
-    yield call([monero_wallet, "setDaemonConnection"], wallet_config.serverUri) // setDaemon Connection
-    let sh2 = yield call([monero_wallet, "getSyncHeight"])
-    console.log("synchheigt before daemon connection:", sh, "after", sh2)
-    return monero_wallet;
-}
+import { monerojs, WalletListener } from "./moneroWalletUtils"
+import { saveWallet } from './saveWalletSaga'
 
 function* workSyncWallet(action) {
 
     //0. get wallet config and check if we need to sync
-    const wallet_config = yield call([storage, "get"], action.payload.name)
+    const wallet_config = yield call(() => db.wallet_config.where({ name: action.payload.name }).first())
 
 
     //1.check if wallet object exists
-    //2.if not load wallet data and open wallet
+    //2.if not load wallet data and open wallet or create wallet if wallet didnt exist before
 
     function isObject(objValue) {
         return objValue && typeof objValue === 'object' && objValue.constructor === Object;
@@ -40,123 +21,51 @@ function* workSyncWallet(action) {
     }
 
 
-
     if (Window.wallets[action.payload.name]) {
         monero_wallet = Window.wallets[action.payload.name]
         if (!wallet_config.sync) {
-            //todo stop syncing call this it is async
             yield call([monero_wallet, "stopSyncing"])
-            //todo save wallet data
-            yield call([monero_wallet, "close"])
-            Window.wallets[action.payload.name] = null;
+            yield put(saveWallet(action.payload.name))
             return
         }
 
-    } else { //open monero wallet if it does not exist already
+    } else { //open or create monero wallet if it does not exist already
         if (!wallet_config.sync) {
-            //just return asap, dont open wallet
+            //just return asap, dont open wallet (because syncing is turned off)
             return
         }
-        monero_wallet = yield call(openWallet, wallet_config, action)
+        if (!wallet_config.last_wallet_save_slot) {
+            //CREATE WALLET
+            monero_wallet = yield call([monerojs, "createWalletFull"], {
+                networkType: wallet_config.networkType,
+                password: wallet_config.password,
+                mnemonic: wallet_config.mnemonic,
+                serverUri: wallet_config.serverUri,
+                restoreHeight: wallet_config.restoreHeight
+            })
+            Window.wallets[action.payload.name] = monero_wallet
+            yield put(saveWallet(action.payload.name))
+        }
+        //OPEN WALLET
+        const wallet_data = yield call(() =>
+            db[wallet_config.last_wallet_save_slot].
+                where({ name: action.payload.name }).first())
+
+        monero_wallet = yield call([monerojs, "openWalletFull"], {
+            networkType: wallet_config.networkType,
+            password: wallet_config.password,
+            serverUri: wallet_config.serverUri,
+            keysData: wallet_data.data[0],
+            cacheData: wallet_data.data[1]
+        })
         Window.wallets[action.payload.name] = monero_wallet
     }
-
-    //3.start syncing
-    //4.add monero wallet listener
-
-
-
-    class WalletListener extends monerojs.MoneroWalletListener {
-        constructor() {
-            super();
-        }
-
-        onOutputReceived(output) {
-            let amount = output.getAmount();
-            let txHash = output.getTx().getHash();
-            let isConfirmed = output.getTx().isConfirmed();
-            let isLocked = output.getTx().isLocked();
-            console.log("output received", output, output.getTx())
-
-        }
-        onOutputSpent(output) {
-            console.log("output spent", output, output.getTx())
-        }
-        onBalancesChanged(newBalance, newUnlockedBalance) {
-            console.log("changed balance", newBalance)
-
-        }
-        onSyncProgress(height, startHeight, endHeight, percentDone, message) {
-            console.log("sync percentage done", percentDone, message)
-        }
-    }
-    //let listener = new WalletListener();
-    //console.log("wallet", monero_wallet, listener)
-
+    //3.add monero wallet listener
     // receive notifications when funds are received, confirmed, and unlocked
-    //yield call([monero_wallet, "addListener"], listener)
-    //monero_wallet.addListener(listener)
-    console.log("after addlistener")
-    yield call(start_monero_wallet_sync, monero_wallet)
-
+    yield call([monero_wallet, "addListener"], new WalletListener())
+    //4. start syncing 
     // synchronize in the background every 5 seconds
-    //yield call([monero_wallet, "startSyncing"], 1000)
-
-
-    console.log("after syncing", monero_wallet.getListeners())
-    // monero_wallet.addListener(new class extends monerojs.MoneroWalletListener {
-    //     onSyncProgress(height, startHeight, endHeight, percentDone, message) {
-    //         console.log("sync percentage done", percentDone, message)
-    //     }
-    // })
-    console.log("after second try", monero_wallet.getListeners())
-    monero_wallet.getSyncHeight().then(x => {
-        console.log("getSyncHeight", x)
-        monero_wallet.getHeight().then(x => {
-            console.log("getHeight", x)
-
-
-            monero_wallet.getDaemonConnection().then(x => {
-                console.log("deaemn", x)
-                monero_wallet.getDaemonHeight().then(x => console.log("DAEMONHEIGHT", x))
-                // monero_wallet.sync(new class extends monerojs.MoneroWalletListener {
-
-                //     onNewBlock(height) {
-                //         console.log("itshappening")
-                //         console.log("new block", height)
-                //     }
-
-                //     onSyncProgress(height, startHeight, endHeight, percentDone, message) {
-                //         console.log("sync percentage done", percentDone, message)
-                //     }
-                // }, 1043315).then(z => {
-                //     console.log("sync ", z)
-                //     monero_wallet.isSynced().then(x => console.log("after sync isSynced", x))
-                //     monero_wallet.getTransfers().then(x => console.log("transfers", x))
-                //     monero_wallet.getBalance().then(x => console.log("balance", x))
-                //     monero_wallet.getTxs().then(x => console.log("gettxs", x))
-                //     monero_wallet.getHeight().then(x => console.log("getHeight", x))
-                //     monero_wallet.getSyncHeight().then(x => console.log("getSyncHeight", x))
-                // })
-                monero_wallet.getTransfers().then(x => console.log("deaemn", x))
-                monero_wallet.getTransfers().then(x => console.log("transfers", x))
-                monero_wallet.isConnectedToDaemon().then(x => console.log("isConnectedToDaemon", x))
-                monero_wallet.isSynced().then(x => console.log("isSynced", x))
-                monero_wallet.getPrimaryAddress().then(x => console.log("primaryaddress", x))
-
-
-            })
-
-
-        })
-
-
-    }
-    )
-
-
-
-
+    yield call([monero_wallet, "startSyncing"], 5000)
 }
 
 function* syncWalletSyncSaga() {
