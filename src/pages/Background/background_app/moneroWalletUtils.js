c108b49c7a83978706c7e23d74c71d73cbed17ab
spirobel <spirobel@protonmail.com>
2022-04-11 16:29:36 +0800

    get rid of unused function in moneroWalletUtils. Only walletlistener and monero javascript api left now

--- src/pages/Background/background_app/moneroWalletUtils.js
+++ src/pages/Background/background_app/moneroWalletUtils.js
@@ -1,5 +1,3 @@
-import { ACTIVE_WALLET, ALL_WALLET_KEYS } from "./createWalletSaga";
-import { storage } from '../../../utils/storage'
 import { db } from "../../../utils/dexie_db"
 
 export const monerojs = require("monero-javascript");
@@ -34,47 +32,4 @@ export class WalletListener extends monerojs.MoneroWalletListener {
 
 }
 
-export function getCurrentActiveWallet() {
 
-    return new Promise((resolve) => {
-        getCurrentActiveWalletName().then((awn) => {
-            chrome.storage.local.get([awn], function (aw) {
-                resolve(aw);
-            })
-        })
-
-    })
-}
-
-
-
-export function getCurrentActiveWalletName() {
-    return new Promise((resolve) => {
-        chrome.storage.local.get(ACTIVE_WALLET, function (aw) {
-            resolve(aw[ACTIVE_WALLET]);
-        })
-    })
-}
-
-export function getAllWalletKeys() {
-    return storage.get(ALL_WALLET_KEYS)
-}
-
-export function setAllWalletKeys(awk, name, content) {
-    if (Array.isArray(awk)) { //1.add new name to awk
-        if (awk.includes(name)) {
-            throw "wallet with this name already exists: name: " + name;
-        };
-        awk.push(name)
-    }
-    else {
-        awk = [name]
-    }
-    awk = [...new Set(awk)] //2. remove duplicates from awk
-
-    return storage.set({ //3.set awk, active wallet name and walletname: walletcontent
-        [name]: content,
-        [ACTIVE_WALLET]: name,
-        [ALL_WALLET_KEYS]: awk
-    })
-}
