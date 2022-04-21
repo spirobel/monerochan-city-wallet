d42e7a065ca1490df999514b182297191b233aaa
spirobel <spirobel@protonmail.com>
2022-04-21 23:10:58 +0800

    rename db and fix autoincrement primary keys

--- src/utils/dexie_db.js
+++ src/utils/dexie_db.js
@@ -1,7 +1,7 @@
 // db.js
 import Dexie from 'dexie';
 
-export const db = new Dexie('myDatabase');
+export const db = new Dexie('monerochan_db');
 db.version(1).stores({
     wallet_config: 'name, main_wallet, last_saved_time',
     wallet_data1: 'name',
@@ -9,6 +9,6 @@ db.version(1).stores({
     transactions: '[wallet_name+tx_hash],url, amount, is_incoming, account_index,subaddress_index, address, num_suggested_confirmations, destinations',
     draft_transaction: 'wallet_name',
     subaddress: 'address, wallet_name, account_index, subaddress_index',
-    identities: 'id++, address, tx_hash, wallet_name, username, is_archived, image, display_index, description',
-    clubcards: 'id++, address, tx_hash, url, wallet_name, host_identity_address, clubcard_name, is_archived, image, display_index, description, display_name, notifications'
+    identities: '++id, address, tx_hash, wallet_name, username, is_archived, image, display_index, description',
+    clubcards: '++id, address, tx_hash, url, wallet_name, host_identity_address, clubcard_name, is_archived, image, display_index, description, display_name, notifications'
 });
