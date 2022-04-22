d75fdae64c8d85c1f4948abc6b2101f52b519717
spirobel <spirobel@protonmail.com>
2022-04-22 08:35:35 +0800

    adopt dexie db so that variable names dont have to be rewritten from getTransfers format

--- src/utils/dexie_db.js
+++ src/utils/dexie_db.js
@@ -6,7 +6,7 @@ db.version(1).stores({
     wallet_config: 'name, main_wallet, last_saved_time',
     wallet_data1: 'name',
     wallet_data2: 'name',
-    transactions: '[wallet_name+tx_hash],url, amount, is_incoming, account_index,subaddress_index, address, num_suggested_confirmations, destinations',
+    transactions: '[wallet_name+tx_hash],isConfirmed, url, amount, isIncoming, accountIndex,subaddressIndex, address, numSuggestedConfirmations, destinations',
     draft_transaction: 'wallet_name',
     subaddress: 'address, wallet_name, account_index, subaddress_index',
     identities: '++id, address, tx_hash, wallet_name, username, is_archived, image, display_index, description',
