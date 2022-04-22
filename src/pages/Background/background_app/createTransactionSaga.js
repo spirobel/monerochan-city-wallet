31a3f6d5482a1ea4c3dcf8a066eaf8a5d61a8b16
spirobel <spirobel@protonmail.com>
2022-04-22 18:13:23 +0800

    fix createTransactionSaga

--- src/pages/Background/background_app/createTransactionSaga.js
+++ src/pages/Background/background_app/createTransactionSaga.js
@@ -1,17 +1,18 @@
 import { call, takeEvery } from 'redux-saga/effects'
 import { db } from "../../../utils/dexie_db"
+import { monerojs } from "./moneroWalletUtils"
 
 function* workCreateTransaction(action) {
     const monero_wallet = Window.wallets[action.payload.wallet_name]
     const transaction = yield call([monero_wallet, "createTx"], {
         accountIndex: 0,
         address: action.payload.address,
-        amount: new BigInteger(action.payload.amount * 1000000000000),
+        amount: new monerojs.BigInteger(action.payload.amount * 1000000000000),
         relay: false
     })
 
     yield call(() => db.draft_transaction.put({
-        fee: transaction.getFee(),
+        fee: monerojs.BigInteger.parse(transaction.getFee()).toString(),
         address: action.payload.address,
         amount: action.payload.amount,
         wallet_name: action.payload.wallet_name,
