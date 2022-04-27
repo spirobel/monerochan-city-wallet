5a97505f4101250301380042c2a4694d6f4a4e13
spirobel <spirobel@protonmail.com>
2022-04-27 18:38:46 +0800

    receive data from the website

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,16 @@
 import { printLine } from './modules/print';
 
 console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+console.log('Must reload extension for modifications to take effect.EFECT2');
 
+window.addEventListener("message", function (event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        console.log("Content script received: " + event.data.text);
+        //port.postMessage(event.data.text);
+    }
+}, false);
 printLine("Using the 'printLine' function from the Print Module");
