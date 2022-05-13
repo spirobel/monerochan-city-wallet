d4c47b0fd773689d63f01e800ad1ce38f5bb0726
spirobel <spirobel@protonmail.com>
2022-05-13 18:11:40 +0800

    wallet entry point

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ var options = {
     newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
+    wallet: path.join(__dirname, 'src', 'pages', 'Wallet', 'index.jsx'),
     background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
   },
@@ -270,6 +271,12 @@ var options = {
       chunks: ['popup'],
       cache: false,
     }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'Wallet', 'index.html'),
+      filename: 'wallet.html',
+      chunks: ['wallet'],
+      cache: false,
+    })
   ],
   infrastructureLogging: {
     level: 'info',
