99a7a972594ee4b8443fcb09bfc75feacc89a116
spirobel <spirobel@protonmail.com>
2022-05-17 22:52:08 +0800

    display clubcard in buy clubcard prompt

--- src/features/buy-clubcards/BuyClubcard.jsx
+++ src/features/buy-clubcards/BuyClubcard.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Button, Input, InputNumber, Form, Card, Space, Spin } from 'antd';
+import { Button, Input, InputNumber, Form, Card, Space, Spin, Image, Alert } from 'antd';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from '../../utils/dexie_db';
 import { RightCircleOutlined } from '@ant-design/icons';
 import { dispatchBackground } from '../../utils/dispatchBackground';
 import { createTransaction } from '../../pages/Background/background_app/createTransactionSaga';
 import { relayTransaction } from '../../pages/Background/background_app/relayTransactionSaga';
+import classNames from 'classnames';
+const { Meta } = Card;
 
 
 export function BuyClubcard(props) {
@@ -35,46 +37,33 @@ export function BuyClubcard(props) {
             {loading && <Spin size="large" tip="Creating transaction..." />
             }
             {(!loading && !draft_transaction) &&
-                <Form
-                    name="save-wallet"
-                    form={form}
-                    onFinish={onFinish}
-                    autoComplete="off"
-                    initialValues={{ amount: 1 }}
-                >
-                    <Form.Item
-                        label="address"
-                        name="address"
-                        rules={[{ required: true, message: 'Please input the desitionation address!' },]}
-                    >
-                        <Input />
-                    </Form.Item>
+                <>
+                    <div className={classNames("ClubcardGrid")}>
+                        <Card
+                            className={classNames("Clubcard")}
+                            hoverable
+                            bordered
+                            cover={
+                                <Image
+                                    preview={false}
+                                    src={props?.clubcard?.image}
+                                />
+                            }
+                        >
+                            <Meta
+                                description={props?.clubcard?.description}
+                            />
+                        </Card>
 
-                    <Form.Item
-                        label="amount"
-                        name="amount"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input the transaction amount!',
-                            },
-                        ]}
-                    >
-                        <InputNumber
-                            style={{
-                                width: 100,
-                            }}
-                            min="0"
-                            step="0.1"
-                            stringMode
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Create transaction
-                        </Button>
-                    </Form.Item>
-                </Form>
+                    </div>
+                    <Alert
+                        message={<span style={{ color: "rgba(0, 0, 0, 0.45)" }}>click on the card to buy</span>}
+                        description={<><span>url: {(new URL(props.clubcard.url)).hostname}</span><br></br><span> price: {props.clubcard.amount}</span></>}
+                        type="info"
+                        showIcon
+                        style={{ margin: "49px" }}
+                    />
+                </>
             }
             {draft_transaction &&
                 <Card title="New transaction" style={{ width: 300 }}>
