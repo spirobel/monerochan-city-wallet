d35c5bd4627806d5809c66a7e291f20377f25819
spirobel <spirobel@protonmail.com>
2022-05-19 12:42:34 +0800

    relay transaction by id

--- src/pages/Background/background_app/relayTransactionSaga.js
+++ src/pages/Background/background_app/relayTransactionSaga.js
@@ -4,12 +4,18 @@ import { saveTransaction } from './moneroWalletUtils'
 import { saveWallet } from './saveWalletSaga'
 
 function* workRelayTransaction(action) {
-    const transaction = yield call(() => db.draft_transaction.where({ wallet_name: action.payload.wallet_name }).first())
-    const monero_wallet = Window.wallets[action.payload.wallet_name]
+    const transaction = yield call(() => db.draft_transaction.where({ id: action.payload.draft_transaction_id }).first())
+    let main_wallet = yield call(() => db.wallet_config.orderBy('main_wallet').last())
+    let monero_wallet = Window.wallets[main_wallet.name]
+    while (!monero_wallet) {
+        delay(300)
+        main_wallet = yield call(() => db.wallet_config.orderBy('main_wallet').last())
+        monero_wallet = Window.wallets[main_wallet.name]
+    }
     const tx_hash = yield call([monero_wallet, "relayTx"], transaction.metadata)
-    yield put(saveWallet(action.payload.wallet_name))
-    yield call(saveTransaction, action.payload.wallet_name, tx_hash)
-    yield call(() => db.draft_transaction.delete(action.payload.wallet_name))
+    yield put(saveWallet(main_wallet.name))
+    yield call(saveTransaction, main_wallet.name, tx_hash)
+    yield call(() => db.draft_transaction.delete(action.payload.draft_transaction_id))
 }
 
 function* relayTransactionSaga() {
@@ -17,11 +23,11 @@ function* relayTransactionSaga() {
 }
 
 
-export function relayTransaction(wallet_name) {
+export function relayTransaction(draft_transaction_id) {
     return {
         type: RELAY_TRANSACTION,
         payload: {
-            wallet_name
+            draft_transaction_id
         }
     }
 }
