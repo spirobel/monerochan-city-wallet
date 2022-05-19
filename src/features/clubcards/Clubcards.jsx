c044832a969b598e98ebec95b770450575466375
spirobel <spirobel@protonmail.com>
2022-05-19 22:35:44 +0800

    save new order of cards when rearranging via DnD

--- src/features/clubcards/Clubcards.jsx
+++ src/features/clubcards/Clubcards.jsx
@@ -20,17 +20,38 @@ import { db } from '../../utils/dexie_db'
 import styles from './Clubcards.module.scss';
 
 export function Clubcards() {
-    const [items, setItems] = useState(['1', '2', '3', '4', '5', '6', '7', '8']);
+    const [items, setItems] = useState([]);
     const sensors = useSensors(
         useSensor(PointerSensor),
         useSensor(KeyboardSensor, {
             coordinateGetter: sortableKeyboardCoordinates,
         })
     );
+    const main_wallet = useLiveQuery(
+        () => db.wallet_config.orderBy('main_wallet').last()
+    );
     const clubcards = useLiveQuery(
         async () => {
-            return await db.clubcards.where({ bought: 1 }).toArray()
+            if (!main_wallet) { return; }
+            let card_list = main_wallet.card_list
+            if (!card_list || typeof card_list[0] !== 'string') {
+                //TODO add wallet_name to clubcards
+                let card_list_array = await db.clubcards.where({ bought: 1 }).toArray()
+                card_list = [];
+                card_list_array.forEach(function (item) {
+                    card_list.push(item.url);
+                });
+
+                console.log("card_list_keys", card_list)
+
+                await db.wallet_config.update(main_wallet.name, {
+                    card_list
+                })
+            }
+            console.log("card_list", card_list)
+            setItems(() => card_list)
         },
+        [main_wallet, setItems]
     );
     console.log("bought clubcards", clubcards)
 
@@ -58,8 +79,11 @@ export function Clubcards() {
             setItems((items) => {
                 const oldIndex = items.indexOf(active.id);
                 const newIndex = items.indexOf(over.id);
-
-                return arrayMove(items, oldIndex, newIndex);
+                const newArray = arrayMove(items, oldIndex, newIndex);
+                db.wallet_config.update(main_wallet.name, {
+                    card_list: newArray
+                })
+                return newArray
             });
         }
     }
